c3ea4d9974497465a59a01707bc49174e372eea9
devemre <dev.emre.aydin@gmail.com>
2024-12-11 15:38:49 +0300

    Section 9: Building the User Interface for the shop

---
+++ client/src/app/features/shop/product-item/product-item.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../../../shared/models/product';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+} from '@angular/material/card';
+import { CurrencyPipe } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
+
+@Component({
+  selector: 'app-product-item',
+  imports: [
+    MatCard,
+    MatCardContent,
+    MatCardActions,
+    MatIcon,
+    MatButton,
+    CurrencyPipe,
+  ],
+  templateUrl: './product-item.component.html',
+  styleUrl: './product-item.component.scss',
+})
+export class ProductItemComponent {
+  @Input() product?: Product;
+}
---
+++ client/src/app/shared/models/shopParams.ts
@@ -0,0 +1,8 @@
+export class ShopParams {
+  brands: string[] = [];
+  types: string[] = [];
+  sort: string = 'name';
+  pageSize: number = 6;
+  pageIndex: number = 1;
+  search: string = '';
+}
