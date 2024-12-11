bbf6f8d930490d5f8ebaf183648f8cd0facbaa4b
devemre <dev.emre.aydin@gmail.com>
2024-12-11 14:09:19 +0300

    Section 8: Angular Basics

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1c691673b08257a8826ff77edf20fa020335f67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7f3ca47605d43c31388683e4c96ef85d0cd84c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
