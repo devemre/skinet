65bfe11a83fbe33d8697d3180d40611a5ea717a8
devemre <dev.emre.aydin@gmail.com>
2024-12-11 21:29:18 +0300

    Section 11: Client side error handling and loading

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { ShopComponent } from './features/shop/shop.component';
 import { ProductDetailsComponent } from './features/shop/product-details/product-details.component';
+import { TestErrorComponent } from './features/test-error/test-error.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { ServerErrorComponent } from './shared/components/server-error/server-error.component';
 
 export const routes: Routes = [
   {
@@ -15,6 +18,18 @@ export const routes: Routes = [
   {
     path: 'shop/:id',
     component: ProductDetailsComponent,
+  },
+  {
+    path: 'test-error',
+    component: TestErrorComponent,
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
+  },
+  {
+    path: 'server-error',
+    component: ServerErrorComponent,
   },
   {
     path: '**',
